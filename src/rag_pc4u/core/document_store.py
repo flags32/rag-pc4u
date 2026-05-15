ac22bf3fb26da92090681e58df8dbb01dc6cd671
flags32 <drop.animer@gmail.com>
2026-05-15 14:58:20 +0200

    finalisation architecture

--- src/rag_pc4u/core/document_store.py
+++ src/rag_pc4u/core/document_store.py
@@ -15,3 +15,26 @@ document_store.write_documents(#fonction de test
 )
 print(document_store.count_documents())
 assert document_store.count_documents() == 2
+"""ici version memoire vive """
+"""
+cependant on peut aussi utiliser cettee version en mémoire persistante :
+    from haystack.dataclasses.document import Document
+    from haystack_integrations.document_stores.qdrant import QdrantDocumentStore
+    from haystack.utils import Secret
+    
+    document_store = QdrantDocumentStore(
+        url="https://XXXXXXXXX.us-east4-0.gcp.cloud.qdrant.io:6333",
+        index="your_index_name",
+        embedding_dim=1024,  # based on the embedding model
+        recreate_index=True,  # enable only to recreate the index and not connect to the existing one
+        api_key=Secret.from_token("YOUR_TOKEN"),
+    )
+    
+    document_store.write_documents(
+        [
+            Document(content="This is first", embedding=[0.0] * 5),
+            Document(content="This is second", embedding=[0.1, 0.2, 0.3, 0.4, 0.5]),
+        ],
+    )
+    print(document_store.count_documents())
+"""
