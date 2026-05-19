b97b1de3a37c847cbe31f267269bb0222b263bee
flags32 <drop.animer@gmail.com>
2026-05-19 15:06:18 +0200

    finition du core

--- src/rag_pc4u/core/document_store.py
+++ src/rag_pc4u/core/document_store.py
@@ -1,5 +1,7 @@
+import structlog
 from haystack.dataclasses.document import Document
 from haystack_integrations.document_stores.qdrant import QdrantDocumentStore
+from rag_pc4u.core.config import settings
 
 document_store = QdrantDocumentStore(#fonction qui sert a tester le document store
     ":memory:",#memory signifiant que les données sont stockées en mémoire vive
@@ -15,29 +17,24 @@ document_store.write_documents(#fonction de test
 )
 print(document_store.count_documents())
 assert document_store.count_documents() == 2
-"""ici il n'y a que la version avec la memoire vive """
 
-"""
-cependant on peut aussi utiliser cettee version en mémoire persistante :
-    from haystack.dataclasses.document import Document
-    from haystack_integrations.document_stores.qdrant import QdrantDocumentStore
-    from haystack.utils import Secret
-    
-    document_store = QdrantDocumentStore(
-        url="https://XXXXXXXXX.us-east4-0.gcp.cloud.qdrant.io:6333",
-        index="your_index_name",
-        embedding_dim=1024,  # based on the embedding model
-        recreate_index=True,  # enable only to recreate the index and not connect to the existing one
-        api_key=Secret.from_token("YOUR_TOKEN"),
-    )
-    
-    document_store.write_documents(
-        [
-            Document(content="This is first", embedding=[0.0] * 5),
-            Document(content="This is second", embedding=[0.1, 0.2, 0.3, 0.4, 0.5]),
-        ],
-    )
-    print(document_store.count_documents())
-"""
+logger = structlog.get_logger(__name__)
 
-"""il faut que j'utilise ça QdrantHybridRetriever mais pour ça il me faut un space vector soit un espace vectoriel"""
+def init_database():
+    """Initialise la collection avec le schéma hybride complet (Dense + Sparse)."""
+    logger.info("Vérification et création de la collection hybride...")
+
+    # En passant recreate_index=True une première fois (ou si tu lances une démo),
+    # Qdrant va paramétrer l'espace hybride automatique
+    ds = QdrantDocumentStore(
+        url=settings.qdrant_url,
+        index=settings.collection_name,
+        embedding_dim=settings.embedding_dim,
+        use_sparse_embeddings=True, # Active la configuration du Named Vector "sparse-text" dans Qdrant
+        recreate_index=True         # ATTENTION: À passer à False en production pour ne pas vider tes données
+    )
+
+    logger.info("Collection initialisée avec succès !", total_docs=ds.count_documents())
+
+if __name__ == "__main__":
+    init_database()
